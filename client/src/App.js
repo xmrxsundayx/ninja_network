6ed1e6115df2a0ddae7c12c7ebeaf2b72bfbca16
Jared Sunday <jaredpatterson07@gmail.com>
2023-06-22 16:57:17 -0700

    Added routes to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,32 @@ import React from 'react';
 import Home from './components/Home';
 import Friends from './components/Friends';
 import Profile from './components/Profile';
-import LoginRegistration from './components/LoginRegistration';
 import Navbar from './components/Navbar';
+import LoginRegistration from './components/LoginRegistration';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <LoginRegistration />
-        <Home />
-        <Friends />
-        <Profile />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginRegistration />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <div className="container">
+                <Route path="/home" element={<Home />} />
+                <Route path="/myninjas" element={<Friends />} />
+                <Route path="/profile/:id" element={<Profile />} />
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
