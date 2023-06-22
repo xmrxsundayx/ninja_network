af53d929813a43a3897d77bd5cadf212633d8db5
Sandy Thai <banyxservices@gmail.com>
2023-06-22 16:50:42 -0700

    pics

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -1,6 +1,9 @@
+// Sandy's component
 import React, { useEffect, useState } from 'react';
 import api from '../api/dummy';
 
+
+// Sample code to test api
 const Friends = () => {
     const [friends, setFriends] = useState([]);
 
@@ -20,12 +23,42 @@ const Friends = () => {
 
     return (
         <div>
-            <h2>All Ninjas</h2>
-            <ul>
-                {friends.map((friend) => (
-                    <li key={friend.id}>{friend.firstName} {friend.lastName}</li>
-                ))}
-            </ul>
+            <div>
+                <h2>My Ninjas</h2>
+                {/* {friends.length === 0 ? ( */}
+                <p>Add Some Ninjas, My Ninja...</p>
+                {/* ) : (
+        <></>
+          )} */}
+            </div>
+            <div>
+                <h2>All Ninjas</h2>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {friends.map((friend) => (
+                        <div key={friend.id} style={{ width: '50%', padding: '20px' }}>
+                            <img
+                                src={friend.picture}
+                                alt={`${friend.firstName} ${friend.lastName}`}
+                                style={{ width: '100px', height: 'auto', margin: '20px' }}
+                            />
+                            {friend.firstName} {friend.lastName}
+                        </div>
+                    ))}
+                </div>
+                <h2>All Ninjas</h2>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {friends.map((friend) => (
+                        <div key={friend.id} style={{ width: '50%', padding: '20px' }}>
+                            <img
+                                src={friend.picture}
+                                alt={`${friend.firstName} ${friend.lastName}`}
+                                style={{ width: '100px', height: 'auto', margin: '20px' }}
+                            />
+                            {friend.firstName} {friend.lastName}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
