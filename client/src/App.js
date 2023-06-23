77a0c98fc405cce1fc7c8ef71b7c980a925c444d
Jared Sunday <jaredpatterson07@gmail.com>
2023-06-22 17:23:35 -0700

    fixed the routes for now

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,43 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginRegistration />} />
-        <Route
-          path="/"
-          element={
-            <div>
-              <Navbar />
-              <div className="container">
-                <Route path="/home" element={<Home />} />
-                <Route path="/myninjas" element={<Friends />} />
-                <Route path="/profile/:id" element={<Profile />} />
-              </div>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="container">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Navigate to="/login" />} path="/" />
+          <Route element={<LoginRegistration />} path='/login' />
+          <Route element={<Home />} path='/home' />
+          <Route element={<Friends />} path='/myninjas' />
+          <Route element={<Profile />} path='/profile/:id' />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/login" />} />
+//         <Route path="/login" element={<LoginRegistration />} />
+//         <Route
+//           path="/"
+//           element={
+//             <div>
+//               <Navbar />
+//               <div className="container">
+//                 <Route path="/home" element={<Home />} />
+//                 <Route path="/myninjas" element={<Friends />} />
+//                 <Route path="/profile/:id" element={<Profile />} />
+//               </div>
+//             </div>
+//           }
+//         />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
