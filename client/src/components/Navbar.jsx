63ef92b58b7947f07899d78c72e76996d10612d3
Jared Sunday <jaredpatterson07@gmail.com>
2023-06-22 19:09:15 -0700

    updated navbar and started styling the html

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import logo from "../images/ninja_network_logo.PNG"
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Ninja Network</h1>
-            <ul>
-                <li><a href="#"/>Home</li>
-                <li><a href="#"/>My Ninjas</li>
-                <li><a href="#"/>Profile</li>
-                <li><a href="#"/>Log Out</li>
-            </ul>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a href="/" class="navbar-brand">
+                        <h1><u>NINJA NETWORK</u></h1>
+                        {/* ********************** */}
+                        {/* if we want to add a logo, we can save the image in the image file and upload it here. Make sure to uncomment it import for the logo as well. */}
+                        {/* <img src={logo} height="28" alt="ninja_logo"/> */}
+                        {/* *********************** */}
+                    </a>
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <div class="navbar-nav">
+                            <a href="/home" class="nav-item nav-link active">Home</a>
+                            <a href="/profile" class="nav-item nav-link">Profile</a>
+                            <a href="/myninjas" class="nav-item nav-link">My Ninjas</a>
+                        </div>
+                        <div class="navbar-nav ms-auto">
+                            <a href="login" class="nav-item nav-link">Log Out</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 }
