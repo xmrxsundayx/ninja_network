8a8f7e5c947c55ebb8e2242571ffa9c29b10652f
Sandy Thai <banyxservices@gmail.com>
2023-06-24 10:35:34 -0700

    correcting :userId to  :id in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,9 +18,9 @@ function App() {
           <Route element={<Login />} path='/login' />
           <Route element={<Register />} path='/register' />
           <>
-          <Route element={<Home />} path='/home/:userId' />
-          <Route element={<Friends />} path='/myninjas/:userId' />
-          <Route element={<Profile />} path='/profile/:userId' />
+          <Route element={<Home />} path='/home/:id' />
+          <Route element={<Friends />} path='/myninjas/:id' />
+          <Route element={<Profile />} path='/profile/:id' />
           {/* I took id off of profile until we can get some useParams in to capture the ID.  */}
           </>
         </Routes>
