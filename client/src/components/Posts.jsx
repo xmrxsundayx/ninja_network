37e84ab7ab19b5df1e73e13af1af22b559a6afc5
Khrista K <khristakhwaja@gmail.com>
2023-06-25 14:31:54 -0700

    post form and posts components

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Posts = () => {
+    const [postList, setPostList] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/posts/all')
+            .then(res => {
+                console.log(res.data);
+                setPostList(res.data)
+            })
+            .catch(err => console.log("Error fetching all posts" + err))
+    }, [])
+
+    const deletePost = (id) => {
+        axios.delete(`http://localhost:8000/api/posts/delete/${id}`)
+            .then(res => {
+                console.log(res.data);
+                setPostList(postList.filter(post => post._id !== id))
+            })
+            .catch(err => console.log("Error deleting post" + err))
+    }
+
+
     return (
-        <div>Posts</div>
+        <div>
+            
+        </div>
     )
 }
 
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -3,6 +3,7 @@ const PostController = require('../controllers/post.controller');
 module.exports = app => {
     app.post('/api/post/create', PostController.createPost);
     app.get('/api/posts/all', PostController.getAllPosts);
+    app.get('/api/post/:id', PostController.getOnePost);
     app.get('/api/posts/user/:id', PostController.getUserPosts);
     app.put('/api/post/update/:id', PostController.updatePost);
     app.delete('/api/post/delete/:id', PostController.deletePost);
